061b2dbb23c90ac141139c91435e55787c0cdef6
emoen <endremoen@hotmail.com>
2021-01-28 21:03:24 +0100

    plot autocorrelation function and autocovariance

--- hw1.r
+++ hw1.r
@@ -7,10 +7,12 @@ presence of a strong seasonal component with period 12 is evident in the graph o
 autocorrelation function.
 '''
 
-data<-read.table("../../Data/deaths.txt", skip = 9, header=FALSE)
-names(data)<-c("month","year","deaths")
-data$time <- as.Date(paste(data$year,data$month,"15",sep="-"))
+data = read.table("C:/prosjekt/Time_Series_stat211/data/deaths.txt", skip = 9, header=FALSE)
+names(data) = c("month","year","deaths")
+data$time = as.Date(paste(data$year,data$month,"15",sep="-"))
 attach(data)
 plot(time,deaths/1000, type="l", xlab="", ylab = "deaths (thousands)")
 
-acf(deaths ,lag.max = 24)
+acf(deaths ,lag.max = 24) #computes (and by default plots) estimates of the autocovariance or autocorrelation function
+
+hist(deaths, breaks = 15)
